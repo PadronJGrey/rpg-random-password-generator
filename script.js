3b8ae5627c89d562269f68d07d3d098209b3e4f6
PadronJGrey <padronj.work@gmail.com>
2023-04-20 14:47:10 -0400

    Re-wrote some of the code that had been destroyed when transferring over

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// ## Acceptance Criteria
-
-// ```
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// ```
 
 // Set all possible variables for generator
 const lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -98,3 +80,16 @@ function generatePassword() {
   return result.join("");
 }
 // Function is done for overall generation of passwords
+
+
+let generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
+// Re-wrote some of the code originally listed for assignment
