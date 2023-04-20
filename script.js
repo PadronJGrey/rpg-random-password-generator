582bbe75576dc217864e8dde3a02e65aeeba6bd5
PadronJGrey <padronj.work@gmail.com>
2023-04-20 13:54:09 -0400

    Set all possible characters for the password generator to pull from

--- script.js
+++ script.js
@@ -18,17 +18,8 @@
 // THEN the password is either displayed in an alert or written to the page
 // ```
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Set all possible variables for generator
+const lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbersChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const symbolsChars = ["!", "@", "#", "$", "%", "&", "*", "(", ")"];
